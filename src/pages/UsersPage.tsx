b53d4b4ce5b114811cb48b78d401d1e90b911fd3
Сергей Политаев <ronin@sonikelf.ru>
2025-10-01 19:35:08 +0300

    feat(users): add sorting by username and email and add pagination

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Spin, Table, Tag } from 'antd';
 import type { TableProps } from 'antd';
 import { useDeleteUserMutation, useGetUsersQuery } from '../store/Users/api';
-import type { User } from '../types/types';
+import type { User, UserFilters } from '../types/types';
 import { Link } from 'react-router-dom';
 import { DeleteOutlined } from '@ant-design/icons';
 
 const UsersPage: React.FC = () => {
+  const [deleteUser, { isLoading: isDeletingUser }] = useDeleteUserMutation();
+
+  const [userFilters, setUserFilters] = useState<UserFilters>({ limit: 10 });
   const {
     data: usersData,
     isLoading: isLoadingUsers,
+    isFetching: isFetchingUsers,
     error,
-  } = useGetUsersQuery({});
-  const [deleteUser, { isLoading: isDeletingUser }] = useDeleteUserMutation();
+  } = useGetUsersQuery({
+    sortBy: userFilters?.sortBy,
+    sortOrder: userFilters?.sortOrder,
+    limit: userFilters?.limit,
+    page: userFilters?.page,
+  });
 
   const columns: TableProps<User>['columns'] = [
     {
       title: 'Имя пользователя',
       dataIndex: 'username',
       key: 'username',
+      sorter: true,
     },
     {
       title: 'Email',
       dataIndex: 'email',
       key: 'email',
+      sorter: true,
     },
     {
       title: 'Дата регистрации',
@@ -91,6 +101,37 @@ const UsersPage: React.FC = () => {
     },
   ];
 
+  const handleTableChange: TableProps<User>['onChange'] = (
+    pagination,
+    __,
+    sorter
+  ) => {
+    let sortOrder: 'asc' | 'desc' | undefined;
+    let sortBy: string | undefined;
+    if (!Array.isArray(sorter)) {
+      switch (sorter.order) {
+        case 'ascend':
+          sortOrder = 'asc';
+          break;
+        case 'descend':
+          sortOrder = 'desc';
+          break;
+        default:
+          sortOrder = undefined;
+          break;
+      }
+
+      sortBy = String(sorter.field);
+    }
+
+    setUserFilters({
+      sortOrder,
+      sortBy,
+      limit: pagination.pageSize,
+      page: pagination.current ? pagination.current - 1 : undefined,
+    });
+  };
+
   if (isLoadingUsers) {
     return <Spin />;
   }
@@ -100,7 +141,20 @@ const UsersPage: React.FC = () => {
   }
 
   return usersData ? (
-    <Table<User> columns={columns} dataSource={usersData.data} rowKey='id' />
+    <Table<User>
+      size='small'
+      columns={columns}
+      dataSource={usersData.data}
+      onChange={handleTableChange}
+      loading={isFetchingUsers}
+      pagination={{
+        position: ['topCenter', 'bottomCenter'],
+        total: usersData?.meta.totalAmount,
+        current: userFilters?.page ? userFilters?.page + 1 : undefined,
+        responsive: true,
+      }}
+      rowKey='id'
+    />
   ) : (
     <div>список пуст</div>
   );
